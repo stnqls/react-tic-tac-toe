ea15a641b2dfa0f7334a81aa985a788eef4013da
unknown <stnqls3938@gmail.com>
2021-09-02 14:49:41 +0900

    onClick & onKeyUp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+
+  const onSubmit = () => {
+    alert('submitted');
+  };
+  const onKeyUp =(event) => {
+    if (event.keyCode === 13) { //enter의 키 코드가 13이다.
+      onSubmit();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onKeyUp={onKeyUp} />
+      <button onClick={onSubmit}>Submit</button>
+      <button onClick={()=> alert('hello')}>hello</button>
+
     </div>
-  );
+    );
 }
 
 export default App;
