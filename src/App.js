cd5771b295e7b20f4165665611f84099d03fc020
unknown <stnqls3938@gmail.com>
2021-09-02 17:22:39 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 
 function App() {
-  const [username, setUsername] = useState(''); // [변수, 변수를 업데이트할 함수]
-  const [password, setPassword] = useState('');
-  const onSubmit = (event) => {
-    event.preventDefault(); // form submit의 고유 특성을 무시하게 해준다.
-    alert('submitted');
+  const [count, setCount] = useState(0);
+  const [hello, setHello] = useState(0);
+  useEffect(()=> {
+    console.log(count);
+  },[count])
+
+  useEffect(()=> {
+    console.log('first rendering')
+  },[])// 빈 배열을 넣어주면 한번만 실행된다.
+
+  const increment = () => {
+    setCount(count+1);
   }
   return (
-      // form이 submit을 하게되면 브라우저는 refresh된다.
     <div className="App">
-      <form onSubmit={onSubmit}> 
-        <input
-          placeholder="Username" 
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)} //e.target.value: input이 가지고있는 값
-        /> 
-        <br/>
-        <input
-          placeholder="Password" 
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          /> 
-        <br/>
-        <button type='submit'>Login</button>
-      </form>
+      <h1>Hello</h1>
+      <button onClick={increment}>Click</button>
+      <button onClick={() => setHello(hello +1 )}>Click1</button>
     </div>
     );
 }
