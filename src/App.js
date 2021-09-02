b9df2343e2de87540d0fffbfd00b19a82f617e56
unknown <stnqls3938@gmail.com>
2021-09-02 18:51:42 +0900

    map사용하기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 function App() {
-  const [condition, setCondition] = useState(false);
-  const toggle = () => {
-    setCondition(!condition);
-  }
-  let renderCondition = condition ? 'True' : 'False'
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <div>
-        {renderCondition}
-      </div>
-      <button onClick={toggle}>Toggle</button>
+  const movies = [
+    {title: 'movie1', year: 2001},
+    {title: 'movie2', year: 2002},
+    {title: 'movie3', year: 2003},
+  ];
+  const renderMovies = movies.map((movie)=> {
+    return (
+      <div className="movie" key={movie.title}>
+      <div className="movie-title">{movie.title}</div>
+      <div className="movie-year">{movie.year}</div>
     </div>
+    );
+  });
+  return (
+  <div className="App">
+    <h1>Movie List</h1>
+    {renderMovies}
+    {/* <div className="movie">
+      <div className="movie-title">{movies[0].title}</div>
+      <div className="movie-year">{movies[0].year}</div>
+    </div> */}
+
+  </div>
     );
 }
 
