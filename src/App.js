44ee674b548908debbfaf57df7eb28e5c20e52e3
unknown <stnqls3938@gmail.com>
2021-09-02 17:37:05 +0900

    components 나누기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React,{useEffect, useState} from 'react';
-
+import React,{ useState} from 'react';
+import Counter from './components/Counter';
 function App() {
-  const [count, setCount] = useState(0);
-  const [hello, setHello] = useState(0);
-  useEffect(()=> {
-    console.log(count);
-  },[count])
-
-  useEffect(()=> {
-    console.log('first rendering')
-  },[])// 빈 배열을 넣어주면 한번만 실행된다.
-
-  const increment = () => {
-    setCount(count+1);
-  }
+  
   return (
     <div className="App">
       <h1>Hello</h1>
-      <button onClick={increment}>Click</button>
-      <button onClick={() => setHello(hello +1 )}>Click1</button>
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
     );
 }
