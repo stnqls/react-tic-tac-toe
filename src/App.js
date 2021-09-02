a5a271ad30ccd47432c536963cb751607f93ea88
unknown <stnqls3938@gmail.com>
2021-09-02 15:19:11 +0900

    useState-form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React,{useState} from 'react';
 
 function App() {
-  const [text, setText] = useState('Hello!');
-  // let text = "Hello!!";
-
-  const updateText = ()=> {
-    // text = "Bye~";
-    setText('Bye~');
+  const [username, setUsername] = useState(''); // [변수, 변수를 업데이트할 함수]
+  const [password, setPassword] = useState('');
+  const onSubmit = (event) => {
+    event.preventDefault(); // form submit의 고유 특성을 무시하게 해준다.
+    alert('submitted');
   }
   return (
+      // form이 submit을 하게되면 브라우저는 refresh된다.
     <div className="App">
-      <span>{text}</span>
-      <button onClick={updateText}>Update</button>
+      <form onSubmit={onSubmit}> 
+        <input
+          placeholder="Username" 
+          value={username} 
+          onChange={(e) => setUsername(e.target.value)} //e.target.value: input이 가지고있는 값
+        /> 
+        <br/>
+        <input
+          placeholder="Password" 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          /> 
+        <br/>
+        <button type='submit'>Login</button>
+      </form>
     </div>
     );
 }
