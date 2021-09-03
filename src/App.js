e90f07ed6e22a89fe3fa9fdf6c42b4b72d16ddba
unknown <stnqls3938@gmail.com>
2021-09-03 14:06:34 +0900

    리스트 추가 컴포넌트 분류하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import Movie from './components/Movie';
+import MovieForm from './components/MovieForm';
+
 function App() {
 
-  const [movieTitle, setMovieTitle] = useState();
-  const [movieYear, setMovieYear] = useState();
   const [movies,setMovies] = useState([
     {title: 'hello1', year: 2001},
     {title: 'hello2', year:2002},
@@ -17,35 +17,17 @@ function App() {
     );
   })
 
-  const addMovie = (event)=> {
-    event.preventDefault();
+  const addMovie = (movie)=> {
     setMovies([
       ...movies,
-      {
-        title: movieTitle,
-        year: movieYear,
-      }
-    ])
+      movie
+    ]);
   };
   
   return (
   <div className="App">
     <h1>Movie List</h1>
-    <form onSubmit={addMovie}>
-      <input
-        type='text'
-        value={movieTitle}
-        placeholder="영화제목"
-        onChange= {e=> setMovieTitle(e.target.value)}
-      /><br/>
-      <input
-        type='text'
-        value={movieYear}
-        placeholder="개봉년도"
-        onChange= {e=> setMovieYear(e.target.value)}
-      /><br/>
-      <button type="submit">영화추가</button>
-    </form>
+    <MovieForm addMovie={addMovie}/>
     {renderMovies}
   </div>
     );
